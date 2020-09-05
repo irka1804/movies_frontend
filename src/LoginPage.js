bf3a94d3414327c85f33e611aa7002fbf5a80328
irka1804 <irka1804@mail.ru>
2020-09-05 03:01:45 +0300

    tab to spaces

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -4,88 +4,88 @@ import { withTranslation } from 'react-i18next'
 
 
 function Error({ error }) {
-	return (
-		<div> 
-			{ error }
-		</div>
-	)
+    return (
+        <div> 
+            { error }
+        </div>
+    )
 }
 
 class LoginPage extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			error: '',
-		}
+    constructor(props) {
+        super(props)
+        this.state = {
+            error: '',
+        }
 
-		this.handleSubmit = this.handleSubmit.bind(this)
-	}
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
 
-	validate ({login, password}) {
-		let users = JSON.parse(window.localStorage.getItem('users'))
-		let user_exist = false
-		let error = ''
-		users.forEach((user) => {
-			if (user.login === login) {
-				user_exist = true
-				if (user.password !== password) {
-					error = 'loginPage.errors.wrongPassword'
-				}
-			}	
-		})
-		if (!user_exist) {
-			error = 'loginPage.errors.unknownUser'
-		}
+    validate ({login, password}) {
+        let users = JSON.parse(window.localStorage.getItem('users'))
+        let user_exist = false
+        let error = ''
+        users.forEach((user) => {
+            if (user.login === login) {
+                user_exist = true
+                if (user.password !== password) {
+                    error = 'loginPage.errors.wrongPassword'
+                }
+            }   
+        })
+        if (!user_exist) {
+            error = 'loginPage.errors.unknownUser'
+        }
 
-		return error
-	}
+        return error
+    }
 
-	handleSubmit (props) {
-		let error = this.validate(props)
-		
-		this.setState({ error })
+    handleSubmit (props) {
+        let error = this.validate(props)
+        
+        this.setState({ error })
 
-		if (!error) {
-			window.localStorage.setItem('current_user', props.login)
-		}
-	}
+        if (!error) {
+            window.localStorage.setItem('current_user', props.login)
+        }
+    }
 
-	render () {
-		const { t } = this.props
+    render () {
+        const { t } = this.props
 
-		return (
-			<div>
-				<Form
-					onSubmit={ this.handleSubmit }
+        return (
+            <div>
+                <Form
+                    onSubmit={ this.handleSubmit }
 
-					render={ ({ handleSubmit }) => (
-						<form onSubmit={ handleSubmit }>
-							<div>
-								<label> { t('loginPage.username') } </label>
-								<Field
-									name='login'
-									type='text'
-									component='input'
-								/>
-							</div>
-							<div>
-								<label> { t('loginPage.password') } </label>
-								<Field
-									name='password'
-									type='password'
-									component='input'
-								/>
-							</div>
-							<Error error={ t(this.state.error) }/>
-							<button type='submit'> 
-								{ t('loginPage.signIn') }
-							</button>
-						</form>
-					)}
-				/>
-			</div>
-		)
-	}
+                    render={ ({ handleSubmit }) => (
+                        <form onSubmit={ handleSubmit }>
+                            <div>
+                                <label> { t('loginPage.username') } </label>
+                                <Field
+                                    name='login'
+                                    type='text'
+                                    component='input'
+                                />
+                            </div>
+                            <div>
+                                <label> { t('loginPage.password') } </label>
+                                <Field
+                                    name='password'
+                                    type='password'
+                                    component='input'
+                                />
+                            </div>
+                            <Error error={ t(this.state.error) }/>
+                            <button type='submit'> 
+                                { t('loginPage.signIn') }
+                            </button>
+                        </form>
+                    )}
+                />
+            </div>
+        )
+    }
 }
 
 export default withTranslation()(LoginPage)
--- src/i18n.js
+++ src/i18n.js
@@ -5,22 +5,22 @@ import EN from './locales/en.json'
 import RU from './locales/ru.json'
 
 const resources = {
-	en: {
-		translation: EN,
-	},
-	ru: {
-		translation: RU,
-	},
+    en: {
+        translation: EN,
+    },
+    ru: {
+        translation: RU,
+    },
 }
 
 i18n
-	.use(initReactI18next)
-	.init({
-		resources,
-		lng: "en",
-		interpolation: {
-      		escapeValue: false
-    	}
-	})
+    .use(initReactI18next)
+    .init({
+        resources,
+        lng: "en",
+        interpolation: {
+            escapeValue: false
+        }
+    })
 
 export default i18n
